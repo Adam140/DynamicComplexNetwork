fb4b811ce6171e2f61d8c1f578bb197e99b2b147
Adam140 <kunikowski.a@gmail.com>
2014-01-22 00:31:04 +0200

    experiment base class

---
+++ src/experiments/AverageShortestPath.java
@@ -0,0 +1,16 @@
+package experiments;
+
+import baseElement.Graph;
+
+public class AverageShortestPath extends Experiment {
+
+	public AverageShortestPath(Graph graph) {
+		super(graph);
+	}
+	
+	public void doExperiment()
+	{
+		
+	}
+
+}
---
+++ src/experiments/Experiment.java
@@ -0,0 +1,19 @@
+package experiments;
+
+import baseElement.Graph;
+
+public class Experiment {
+
+	protected Graph graph;
+	protected String result;
+	
+	public Experiment(Graph graph)
+	{
+		this.graph = graph;
+	}
+	
+	public void doExperiment()
+	{
+		
+	}
+}
