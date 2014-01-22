c050b3ee4381bc66843d2e276b4394f8016a774e
Adam140 <kunikowski.a@gmail.com>
2014-01-23 01:57:26 +0200

    network degree

--- src/manager/main.java
+++ src/manager/main.java
@@ -1,11 +1,9 @@
 package manager;
 
-import experiments.AverageDegree;
-import experiments.AverageShortestPath;
-import experiments.Density;
-import experiments.StrongConnectivity;
-import experiments.clusteringCoefficient;
 import baseElement.Graph;
+import experiments.AverageDegree;
+import experiments.ClusteringCoefficient;
+import experiments.StrongConnectivity;
 
 public class main {
 
@@ -14,17 +12,17 @@ public class main {
 		
 		graph.init("data/p2p-Gnutella08.txt");
 		//graph.init("data/test.txt");
-		graph.printAllEdges();
-		graph.displayGraph();
+//		graph.printAllEdges();
+//		graph.displayGraph();
 		//AverageShortestPath e = new AverageShortestPath(graph);
 		//e.doExperiment();
 		//System.out.println(e.getParameter(AverageShortestPath.Parameters.AVERAGE_SHORTEST_PATH));
-		clusteringCoefficient e1 = new clusteringCoefficient(graph);
-		e1.doExperiment();
+//		ClusteringCoefficient e1 = new ClusteringCoefficient(graph);
+//		e1.doExperiment();
 		AverageDegree e2 = new AverageDegree(graph);
 		e2.doExperiment();
-		StrongConnectivity e3 = new StrongConnectivity(graph);
-		e3.doExperiment();
+//		StrongConnectivity e3 = new StrongConnectivity(graph);
+//		e3.doExperiment();
 	}
 
 }
